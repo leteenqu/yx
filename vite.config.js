b6b795c8094daee9fbaf70483c417b5a426dbc9e
leteenqu <152240097+leteenqu@users.noreply.github.com>
2025-05-29 09:35:20 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // 设置资源的基础路径
-});
+  base: './', // ⭐ 建议添加此行以解决资源路径问题
+})
